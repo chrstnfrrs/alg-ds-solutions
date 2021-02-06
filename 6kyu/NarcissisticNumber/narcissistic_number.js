343f7125472a66feeea594c2e9f08d6c72dd6ec6
Christian Farris <christianfarriscs@gmail.com>
2021-02-06 16:38:36 -0600

    6kyu narcissistic number

---
+++ 6kyu/NarcissisticNumber/narcissistic_number.js
@@ -0,0 +1,4 @@
+const narcissistic = (number) => number === String(number).split('').reduce((accumulator, index) => accumulator+=index**String(number).length, 0)
+
+console.log(narcissistic(153))
+console.log(narcissistic(1652))
