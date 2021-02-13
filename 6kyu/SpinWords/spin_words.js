d2d95675400815bc0e4c6d46091e55ead496bd5a
Christian Farris <christianfarriscs@gmail.com>
2021-02-13 14:33:54 -0600

    6kyu spin words

---
+++ 6kyu/SpinWords/spin_words.js
@@ -0,0 +1,10 @@
+const spinWords = (sentence) => sentence.split(' ').map(word => {
+    return word.length > 4 ? word.split('').reverse().join('') : word
+  }).join(' ')
+
+// "Hey wollef sroirraw"
+console.log(spinWords("Hey fellow warriors"))
+// "This is a test"
+console.log(spinWords("This is a test"))
+// "This is rehtona test"
+console.log(spinWords("This is another test"))
