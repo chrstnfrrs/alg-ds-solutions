2a1281115f676bc808cc477725632605157d49c9
Christian Farris <christianfarriscs@gmail.com>
2021-02-13 14:33:47 -0600

    6kyu Expaned Form

---
+++ 6kyu/ExpandedForm/README.md
@@ -0,0 +1,14 @@
+# Problem
+
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+
+If you liked this kata, check out part 2!!
+
+## Link to Kata
+[Link to Kata](https://www.codewars.com/kata/5842df8ccbd22792a4000245/)
---
+++ 6kyu/ExpandedForm/expanded_form.js
@@ -0,0 +1,16 @@
+const getString = (number, value, index) => value.padEnd(String(number).length - index, '0')
+
+const expandedForm = (number) => String(number).split('').reduce((accumulator, value, index) => {
+  if (value !== '0') {
+    if (index !== 0) {
+      accumulator += ' + '
+    } 
+    accumulator += getString(number, value, index)
+  }
+  
+  return accumulator
+}, '')
+
+console.log(expandedForm(12)); // Should return '10 + 2')
+console.log(expandedForm(42)); // Should return '40 + 2')
+console.log(expandedForm(70304)); // Should return '70000 + 300 + 4')
