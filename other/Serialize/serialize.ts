896705be56e04b895f3a093206cb3b312c8afa20
Christian Farris <christianfarriscs@gmail.com>
2021-07-01 01:52:47 -0500

    implement JSON.stringify()

---
+++ other/Serialize/serialize.ts
@@ -0,0 +1,35 @@
+type Serialize = (input: any) => string;
+
+const serialize: Serialize = (input) => {
+  // array
+  if (Array.isArray(input)) {
+    const serializedArray = input
+      .map((item: any) => {
+        return serialize(item);
+      })
+      .join(', ');
+
+    return `[${serializedArray}]`;
+  }
+
+  // object
+  if (typeof input === 'object') {
+    const serializedObject = Object.keys(input)
+      .map((key: any) => {
+        return `"${key}": ${serialize(input[key])}`;
+      })
+      .join(', ');
+
+    return `{${serializedObject}}`;
+  }
+
+  // number
+  if (typeof input === 'number') {
+    return `${input}`;
+  }
+
+  // DEFAULT IS NULL
+  return `"${input}"`;
+};
+
+export { serialize };
