0cecfbe350055e67d72307d1acbcf3443971b83a
Christian Farris <christianfarriscs@gmail.com>
2021-10-15 23:12:45 -0500

    5kyu: Moving Zeros to the End

---
+++ 5kyu/MovingZerosToTheEnd/moving_zeros_to_the_end.js
@@ -0,0 +1,2 @@
+const moveZerosA = (arr) => [...arr.filter((item) => item !== 0), ...arr.filter((item) => item === 0)]
+const moveZerosB = (arr) => arr.filter((item) => item !== 0).concat(arr.filter((item) => item === 0))
