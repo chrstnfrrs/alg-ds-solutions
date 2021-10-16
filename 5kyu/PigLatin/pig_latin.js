561c11aa3cb724531867f5c846e634fad3d2e32f
Christian Farris <christianfarriscs@gmail.com>
2021-10-15 23:12:57 -0500

    5kyu: Simple Pig Latin

---
+++ 5kyu/PigLatin/pig_latin.js
@@ -0,0 +1,18 @@
+const pigIt = (str) => {
+  const arrOfWords = str.split(' ')
+  const firstLetters = []
+  const remainders = []
+  arrOfWords.forEach((word) => {
+    firstLetters.push(word.substring(0, 1))
+    remainders.push(word.substring(1))
+  })
+  
+  const pigLatin = arrOfWords.map((_, i) => {
+    return ['.', '?', '!'].includes(firstLetters[i])
+      ? `${remainders[i]}${firstLetters[i]}`
+      : `${remainders[i]}${firstLetters[i]}ay`
+  })
+
+  return `${pigLatin.join(' ')}`
+        
+}
